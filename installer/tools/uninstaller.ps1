26ca6af856aec581f1c280238e2b727201038263
Fixstars-momoko <momoko.kono@fixstars.com>
2024-02-23 07:47:58 -0800

    added uninstaller script for user-install option

--- installer/tools/uninstaller.ps1
+++ installer/tools/uninstaller.ps1
@@ -1,63 +1,103 @@
-Write-Output "####################################################"
-Write-Output "# THIS SCRIPT REMOVE SDK AND ENVIRONMENT VARIABLES #"
-Write-Output "####################################################"
+function Write-Log{
+    param(
+        [Parameter(ValueFromRemainingArguments=$true)]
+        [string[]]$Arguments
+    )
 
-
-$currentPATH = [Environment]::GetEnvironmentVariable("Path", "User")
-$deleteTarget= [Environment]::GetEnvironmentVariable("SENSING_DEV_ROOT", "User")
-
-
-$deleteTargetBin =  $deleteTarget+"\bin"
-$pathArray = $currentPATH -split ";"
-
-# If $env:PATH has $env:SENSING_DEV_ROOT+"\bin", remove it
-if ($pathArray -contains $deleteTargetBin) {
-    Write-Output "PATH has SENSING_DEV_ROOT."
-    $pathArray = $pathArray | Where-Object { $_ -ne $deleteTargetBin }
-    $newPath = $pathArray -join ";"
-    [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
-    Write-Output "Removed $deleteTargetBin from PATH."
-} else {
-    Write-Output "PATH does not have SENSING_DEV_ROOT. --skip"
-}
-
-$variableName="SENSING_DEV_ROOT"
-if ([Environment]::GetEnvironmentVariable($variableName, "User") -ne $null) {
-    echo [Environment]::GetEnvironmentVariable($variableName, "User")
-    Write-Output "SENSING_DEV_ROOT will be removed."
-    [Environment]::SetEnvironmentVariable($variableName, $null, "User")
-
-    if (Test-Path -Path $deleteTarget) {
-        echo "Directory $deleteTarget will be removed."
-        Remove-Item $deleteTarget -r -force
-    } else {
-        echo "Directory $deleteTarget does not exist. --skip"
+    $ret = ""
+    foreach ($Argument in $Arguments) {
+        $ret = $ret +  $Argument +" "
     }
-} else {
-    Write-Output "SENSING_DEV_ROOT is not set. --skip"
+    Write-Output $ret 
 }
 
-$variableName="PYTHONPATH"
-if ([Environment]::GetEnvironmentVariable($variableName, "User") -ne $null) {
-    echo [Environment]::GetEnvironmentVariable($variableName, "User")
-    Write-Output "PYTHONPATH will be removed."
-    [Environment]::SetEnvironmentVariable($variableName, $null, "User")
-} else {
-    Write-Output "PYTHONPATH is not set. --skip"
+function Check-EnvironmentVariableExistence{
+    param(
+        [string]$EnvValName
+    )
+    return [Environment]::GetEnvironmentVariable($EnvValName, "User") -ne $null
 }
 
-$variableName="GST_PLUGIN_PATH"
-if ([Environment]::GetEnvironmentVariable($variableName, "User") -ne $null) {
-    echo [Environment]::GetEnvironmentVariable($variableName, "User")
-    Write-Output "GST_PLUGIN_PATH will be removed."
-    [Environment]::SetEnvironmentVariable($variableName, $null, "User")
-} else {
-    Write-Output "GST_PLUGIN_PATH is not set. --skip"
+function Remove-EnvironmentVariable{
+    param(
+        [string]$EnvValName,
+        [bool]$IsDirectoryRemoved=$false
+    )
+    Write-Log "Removing from User Environment variable" $EnvValName
+    if (Check-EnvironmentVariableExistence -EnvValName $EnvValName){
+        $ItsPath = [Environment]::GetEnvironmentVariable($EnvValName, "User")
+        Write-Log $EnvValName "exists:" $ItsPath 
+        Write-Log "Removed" $EnvValName "from User Environment variables."
+        [Environment]::SetEnvironmentVariable($EnvValName, $null, "User")
+
+        if ($IsDirectoryRemoved){
+            Remove-Directory -TargetPath $ItsPath
+        }
+    } else {
+        Write-Log $EnvValName " does not exist. --skipped"
+    }
 }
 
-Write-Output "####################################################"
-Write-Output "# THIS SCRIPT REMOVE SDK AND ENVIRONMENT VARIABLES #"
-Write-Output "####################################################"
+function Remove-PathFromPath{
+    param(
+        [string]$TargetPath
+    )
+    if ($TargetPath -ne $null){
+        Write-Log "Removing" $TargetPath "from User Environment variable PATH"
+
+        $currentPATH = [Environment]::GetEnvironmentVariable("Path", "User")
+        $pathArray = $currentPATH -split ";"
+    
+        if ($pathArray -contains $TargetPath) {
+            $pathArray = $pathArray | Where-Object { $_ -ne $deleteTargetBin }
+            $newPath = $pathArray -join ";"
+            [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+            Write-Log "Removed" $TargetPath "from User Environment variable PATH"
+        } else {
+            Write-Log "PATH does not have" $TargetPath ". --skip"
+        }
+    }
+}
+
+function Remove-Directory{
+    param(
+        [string]$TargetPath
+    )
+    Write-Log "Uninstalling" $TargetPath "from this host machine."
+    if (Test-Path -Path $TargetPath) {
+        Remove-Item $TargetPath -r -force
+        Write-Log "Uninstalled" $TargetPath "from this host machine."
+    } else {
+        Write-Log $TargetPath " does not exist. --skipped"
+    } 
+}
+
+Write-Log "####################################################"
+Write-Log "# THIS SCRIPT REMOVE SDK AND ENVIRONMENT VARIABLES #"
+Write-Log "####################################################"
+
+$deleteSensingDev = $true
+
+try { 
+    Get-Package -Name "sensing-dev" -ErrorAction Stop
+
+    Write-Log "Sensing-Dev is installed with msi and you can uninstall it from Control Panel."
+    Write-Log "Uninstall Sensing-Dev --skip"
+    $deleteSensingDev = $false
+} catch {
+    $deleteSensingDev = $true
+}
+
+$SENSING_DEV_BIN = [Environment]::GetEnvironmentVariable("SENSING_DEV_ROOT", "User") + "\bin"
+
+Remove-PathFromPath -TargetPath $SENSING_DEV_BIN
+Remove-EnvironmentVariable -EnvValName "SENSING_DEV_ROOT" -IsDirectoryRemoved $deleteSensingDev
+Remove-EnvironmentVariable -EnvValName "PYTHONPATH"
+Remove-EnvironmentVariable -EnvValName "GST_PLUGIN_PATH"
+
+
+
+
 
 
 
