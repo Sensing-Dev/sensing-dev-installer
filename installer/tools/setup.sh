03a2c8d80c15c70042dcb4c49b6a5c4a568295bf
Kono Momoko (Fixstars Solutions, Inc. momoko.kono) <momoko.kono@fixstars.com>
2026-02-26 13:30:43 -0800

    added coming release versions

--- installer/tools/setup.sh
+++ installer/tools/setup.sh
@@ -51,6 +51,7 @@ declare -A gendc_separator_config=( # Declare an associative array with default 
     ["v24.05.10"]="v0.2.8"
     ["v24.05.11"]="v0.2.8"
     ["v24.05.12"]="v0.2.8"
+    ["v24.05.13"]="v0.2.8"
 )
 
 unset aravis_config
@@ -63,7 +64,8 @@ declare -A aravis_config=( # Declare an associative array with default values
     ["v24.05.09"]="0.8.30-internal"
     ["v24.05.10"]="0.8.30-internal"
     ["v24.05.11"]="v0.8.30-internal.1"
-    ["v24.05.12"]="v0.8.30-internal.1"
+    ["v24.05.13"]="v0.8.30-internal.1"
+    ["v24.05.13"]="v0.8.30-internal.1"
 )
 
 unset aravis_url
